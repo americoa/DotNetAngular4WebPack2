ed7197a2df657e6a021a858ffe484dc8fb475880
asp_mvc5_next <americoarvani@americos-mbp.home>
2017-08-17 22:34:57 +0100

    add second step

--- Startup.cs
+++ Startup.cs
@@ -21,17 +21,17 @@ namespace aarvanilabAngular4
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
+            
+            //webserver to use index.html, default.html
+            app.UseDefaultFiles();
+            //webserver to use static files
+            app.UseStaticFiles();
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            
         }
     }
 }
